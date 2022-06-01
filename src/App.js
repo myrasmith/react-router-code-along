8904ded63e5dc0d2f2859f928686668d73e154fe
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-06-01 08:05:16 -0400

    class period 1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import Footer from './components/common/footer.js'
-import Header from './components/common/header'
-import Nav from './components/common/nav'
-import About from './components/pages/about'
-import Home from './components/pages/home'
-import Newsletter from './components/pages/newsletter'
-import ProductList from './components/products/productList'
-import Product from './components/products/product'
+import Footer from "./components/common/footer.js";
+import Header from "./components/common/header";
+import Nav from "./components/common/nav";
+import About from "./components/pages/about";
+import Home from "./components/pages/home";
+import Newsletter from "./components/pages/newsletter";
+import ProductList from "./components/products/productList";
+import Product from "./components/products/product";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import lamps from './data/lamps.json'
-import candles from './data/candles.json'
-
+import lamps from "./data/lamps.json";
+import candles from "./data/candles.json";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="wrapper">
-      <Nav />
-      <main>
-        <Home />
-        <About />
-        <Newsletter />
-        <ProductList products={lamps} type={'Lamps'}/>
-        <ProductList products={candles} type={'Candles'}/>
-      </main>
-    </div>
-    <Footer />
+      <Router>
+        <Header />
+        <div className="wrapper">
+          <Nav />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/newsletter" element={<Newsletter />} />
+              <Route
+                path="/lamps"
+                element={<ProductList products={lamps} type={"Lamps"} />}
+              />
+              <Route
+                path="/candles"
+                element={<ProductList products={candles} type={"Candles"} />}
+              />
+            </Routes>
+          </main>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
+
+/* 
+              
+              
+              */
 
 export default App;
--- src/components/products/productList.js
+++ src/components/products/productList.js
@@ -1,24 +1,20 @@
-
-
-export default function ProductList({products, type}) {
-    return (
-        <section><h2>All {type}</h2>
-        <article className="products">
-            <ul>
-                {
-                    products.map(product => {
-                        return (
-                            <li key={product.id}>
-                                <h4>{product.name}</h4>
-                                <img src={product.image} alt={product.name} />
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-
-
-        </article>
-        </section>
-    );
-  };
+export default function ProductList({ products, type }) {
+  console.log(products);
+  return (
+    <section>
+      <h2>All {type}</h2>
+      <article className="products">
+        <ul>
+          {products.map((product) => {
+            return (
+              <li key={product.id}>
+                <h4>{product.name}</h4>
+                <img src={product.image} alt={product.name} />
+              </li>
+            );
+          })}
+        </ul>
+      </article>
+    </section>
+  );
+}
